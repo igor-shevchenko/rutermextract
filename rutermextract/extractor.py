660ba7479bff5b5c3de3dd92b563cfcc24c9f4e6
Igor Shevchenko <mail@igorshevchenko.ru>
2014-07-17 23:42:12 +0600

    Добавил в extractor возможность извлекать вложенные тёрмы

--- rutermextract/extractor.py
+++ rutermextract/extractor.py
@@ -2,14 +2,28 @@
 class Extractor(object):
     """
     Извлечение из текста ключевых слов (в виде списка токенов) в соответствии с поданной IOB-последовательностью.
+
+    Если аргумент nested установлен в True, то извлекаются еще и вложенные тёрмы.
+    Например, вместе с "функциональный язык программирования" извлекаются "язык программирования" и "программирование".
     """
-    def __call__(self, tokens, iob_sequence):
-        chunk = []
+    def __call__(self, tokens, iob_sequence, nested=False):
+        # Список блоков, представленых в виде списка токенов.
+        # Если nested равен False, то в списке всегда один блок.
+        chunks = []
+
         for token, label in zip(tokens, iob_sequence):
-            if len(chunk) and label in ['O', 'B']:
+            if len(chunks) and label in ['O', 'B']:
+                for chunk in chunks:
+                    yield chunk
+                chunks = []
+
+            if label == 'I':
+                for chunk in chunks:
+                    chunk.append(token)
+
+            if label == 'B' or nested and label == 'I':
+                chunks.append([token])
+
+        if len(chunks):
+            for chunk in chunks:
                 yield chunk
-                chunk = []
-            if label in ['I', 'B']:
-                chunk.append(token)
-        if len(chunk):
-            yield chunk
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -8,10 +8,19 @@ class ExtractorTest(unittest.TestCase):
         self.extractor = Extractor()
 
     def test_extract(self):
-        self.assertListEqual(list(self.extractor([1, 2, 3, 4, 5, 6], 'BIOBBO')), [[1, 2], [4], [5]])
+        self.assertListEqual(list(self.extractor([1, 2, 3, 4, 5, 6, 7], 'BIIOBBO')), [[1, 2, 3], [5], [6]])
 
     def test_extract_final_chunk(self):
-        self.assertListEqual(list(self.extractor([1, 2, 3, 4, 5, 6], 'BIOBBI')), [[1, 2], [4], [5, 6]])
+        self.assertListEqual(list(self.extractor([1, 2, 3, 4, 5, 6, 7], 'BIIOBBI')), [[1, 2, 3], [5], [6, 7]])
+
+    def test_extract_nested(self):
+        self.assertListEqual(list(self.extractor([1, 2, 3, 4, 5, 6, 7], 'BIIOBBO', nested=True)),
+                             [[1, 2, 3], [2, 3], [3], [5], [6]])
+
+    def test_extract_final_chunk_nested(self):
+        self.assertListEqual(list(self.extractor([1, 2, 3, 4, 5, 6, 7], 'BIIOBBI', nested=True)),
+                             [[1, 2, 3], [2, 3], [3], [5], [6, 7], [7]])
+
 
 
 if __name__ == '__main__':
